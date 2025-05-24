70e3f85229ba935ec064b7eb8ca92c7ea8b74c27
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-05-24 11:25:54 +0100

    Add Java and XML Files License Headers - Meeds-io/meeds#3324 (#104)
    This change will add LGPL License Header and clean useless File Headers.

--- exo.core.component.organization.api/src/main/java/org/exoplatform/services/organization/impl/mock/SimpleMembershipListAccess.java
+++ exo.core.component.organization.api/src/main/java/org/exoplatform/services/organization/impl/mock/SimpleMembershipListAccess.java
@@ -1,20 +1,20 @@
-/*
- * Copyright (C) 2011 eXo Platform SAS.
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exoplatform.services.organization.impl.mock;
 
@@ -24,10 +24,6 @@ import org.exoplatform.services.organization.Membership;
 import java.util.Collection;
 import java.util.Iterator;
 
-/**
- * @author <a href="abazko@exoplatform.com">Anatoliy Bazko</a>
- * @version $Id: SimpleJDBCMembershipListAccess.java 34360 2009-07-22 23:58:59Z tolusha $
- */
 public class SimpleMembershipListAccess implements ListAccess<Membership>
 {
 
