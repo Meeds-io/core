3c6dafbde9905f9f7d4a6c5e016531be27966a55
Anwar khanfir <akhanfir@exoplatform.com>
2026-02-12 08:34:43 +0100

    feat: Review the operation Create User - MEED-10151 - Meeds-io/MIPs#242 This change will treat the following tasks: . Review the Create User operation. . Review the operation Enable User. . Review the operation Disable User. . Allow to specify a time for group members. . Do not deactivate automatically user when platform admin. In addition, this change adds a new String parameter groupId to the disableInactiveUsers method. This groupId represents the ID of the group to which the automatic deactivation will be applied. We also added an extra condition to prevent deactivating users who belong to the admin group. On the analytics side, a new attribute User Source has been added to indicate the original source of user creation. The labels of the other attributes have also been updated.

--- exo.core.component.organization.api/src/main/java/org/exoplatform/services/organization/impl/mock/DummyOrganizationService.java
+++ exo.core.component.organization.api/src/main/java/org/exoplatform/services/organization/impl/mock/DummyOrganizationService.java
@@ -361,7 +361,7 @@ public class DummyOrganizationService extends BaseOrganizationService
       }
 
       @Override
-      public int disableInactiveUsers(int inactiveDays) {
+      public int disableInactiveUsers(String groupId, int inactiveDays) {
          return 0;
       }
 
